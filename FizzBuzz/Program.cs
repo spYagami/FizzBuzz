bb4546a2c1f0736b37c873b3addf0fc40040af02
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-08-04 15:15:47 +0100

    Add completed FizzBuzz exercise

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 30;
+            for(int i=1; i<= 30; i++)
+            {
+                Console.WriteLine($"{FizzBuzz(i)}");
+            }
+        }
+
+        public static string FizzBuzz(int n)
+        {
+            if (n % 3 == 0)
+                return "Fizz";
+            else if (n % 5 == 0)
+                return "Buzz";
+            else if (n % 15 == 0)
+                return "FizzBuzz";
+
+            return n.ToString();
+ 
+        }
+
+        public static string DivisibleByThree(int x)
+        {
+            if (x % 3 == 0)
+                return "Fizz";
+
+            return string.Empty;
+        }
+        public static string DivisibleByFive(int x)
+        {
+            if (x % 5 == 0)
+                return "Buzz";
+
+            return string.Empty;
+        }
+        public static string DivisibleByFifteen(int x)
+        {
+            if (x % 15 == 0)
+                return "FizzBuzz";
+
+            return string.Empty;
+        }
+    }
+}
---
+++ FizzBuzz_Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using FizzBuzz;
+
+namespace FizzBuzz_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(29, "29")]
+
+        public void GivenOne_Return_OneString(int input, string output)
+        {
+            Assert.That(Program.FizzBuzz(input), Is.EqualTo(output));
+        }
+
+        [TestCase(27)]
+        public void GivenANumberDivisibleByThree_Return_Fizz(int input)
+        {
+            Assert.That(() => Program.DivisibleByThree(input), Is.EqualTo("Fizz"));
+        }
+        [TestCase(25)]
+        public void GivenANumberDivisibleByFive_Return_Buzz(int input)
+        {
+            Assert.That(() => Program.DivisibleByFive(input), Is.EqualTo("Buzz"));
+        }
+        [TestCase(225)]
+        public void GivenANumberDivisibleByFifteen_Return_FizzBuzz(int input)
+        {
+            Assert.That(() => Program.DivisibleByFifteen(input), Is.EqualTo("FizzBuzz"));
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(1, "1")]
+        [TestCase(27, "Fizz")]
+        [TestCase(25, "Buzz")]
+        [TestCase(30,"FizzBuzz")]
+        public void GivenAnyValue_Return_RightOutput(int x, string expected)
+        {
+            Assert.That(() => Program.FizzBuzz(x), Is.EqualTo(expected));
+        }
+
+
+    }
+}
